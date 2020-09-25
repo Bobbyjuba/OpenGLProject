a37302fdea06d918edd889ba6c59079b6009d612
Brian Roden <32974706+Bobbyjuba@users.noreply.github.com>
2020-09-25 14:31:53 -0500

    GitHub Bot Test
    This is for testing the GitHub Bot integration with our dev server. But also, it gives me an excuse to push a version that fixes the heretical placement of { in Source.cpp

--- OpenGLProject/src/Source.cpp
+++ OpenGLProject/src/Source.cpp
@@ -1,7 +1,6 @@
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
+int main(void) {
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -10,8 +9,7 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
@@ -20,15 +18,14 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
         glVertex2f( 0.0f,  0.5f);
-        glVertex2f(0.5f, -0.5f);
+        glVertex2f( 0.5f, -0.5f);
         glEnd();
 
         /* Swap front and back buffers */
