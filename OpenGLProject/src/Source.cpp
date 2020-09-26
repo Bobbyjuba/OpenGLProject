4f764ecd95e738b4d6ddffb03cd01273bf31092c
Brian Roden <32974706+Bobbyjuba@users.noreply.github.com>
2020-09-25 21:40:46 -0500

    Formatted for Modern OpenGL
    Linked with GLEW to format the project around using modern OpenGL.

--- OpenGLProject/src/Source.cpp
+++ OpenGLProject/src/Source.cpp
@@ -1,37 +1,32 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void) {
     GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
+    if (glfwInit() == GL_FALSE)
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
+    window = glfwCreateWindow(800, 600, "OpenGLProject", NULL, NULL);
+
+    if (window == GL_FALSE) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)) {
-        /* Render here */
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+
+    while (glfwWindowShouldClose(window) == GL_FALSE) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GL_TRUE);
+
         glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
